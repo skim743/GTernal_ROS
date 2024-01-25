5479664540025c8cf50fbabbc4ba41c0a2693de8
Soobum Kim <skim743@gatech.edu>
2024-01-24 20:41:24 -0500

    Updated docker scripts

---
+++ docker/arm64/docker_push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker tag firmware_ros:latest skim743/gternal-ros
+sudo docker push skim743/gternal-ros
--- docker/arm64/docker_run.sh
+++ docker/arm64/docker_run.sh
@@ -4,4 +4,5 @@ sudo docker run -it \
            --rm \
            --net host \
 	   --device $(sudo -u pi python3 detect_serial.py):/dev/ttyAMA0 \
+	   -w /home/GTernal_ROS \
            skim743/gternal-ros:latest
